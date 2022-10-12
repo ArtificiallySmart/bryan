0206438c6482cf29c7138e847bcb76bf5187f2db
bryan <bryanparcival@gmail.com>
2022-10-12 16:37:31 +0200

    Added fonts and colors

--- apps/plant-app/src/app/app.module.ts
+++ apps/plant-app/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +17,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
     NavbarComponent,
     HomeComponent,
     CollectionComponent,
--- apps/plant-app/src/app/navbar/navbar.component.ts
+++ apps/plant-app/src/app/navbar/navbar.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   constructor() {}
 
+  isCollapsed = true;
+
   ngOnInit(): void {}
 }
